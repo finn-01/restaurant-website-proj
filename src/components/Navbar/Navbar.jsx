605b9c8ac9ded0d223b9629d0935883a120ff27e
finn-01 <nguyensam576@gmail.com>
2022-02-06 14:54:29 +0700

    Navbar.jsx and navbar.css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,127 @@
-import React from 'react';
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
+import images from "../../constants/images";
+import "./Navbar.css";
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
+	return (
+		<nav className="app__navbar">
+			{/* Logo */}
+			<div className="app__navbar-logo">
+				<img src={images.gericht} alt="logo" />
+			</div>
+
+			{/* Mid */}
+			<ul className="app__navbar-links">
+				<li className="p__opensans">
+					<a href="#">Home</a>
+				</li>
+				<li className="p__opensans">
+					<a href="#about">About</a>
+				</li>
+				<li className="p__opensans">
+					<a href="#menu">Menu</a>
+				</li>
+				<li className="p__opensans">
+					<a href="#awards">Awards</a>
+				</li>
+				<li className="p__opensans">
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+
+			{/* Login */}
+			<div className="app__navbar-login">
+				<a href="#login" className="p__opensans">
+					Login / Registration
+				</a>
+				<div />
+				<a href="/" className="p__opensans">
+					Book Table
+				</a>
+			</div>
+
+			{/* Smalls Screen */}
+			<div className="app__navbar-smallscreen">
+				<GiHamburgerMenu
+					color="#fff"
+					fontSize={27}
+					onClick={() => {
+						setToggleMenu(true);
+					}}
+				/>
+
+				{toggleMenu && (
+					<div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+						<MdOutlineRestaurantMenu
+							fontSize={27}
+							className="overlay__close"
+							onClick={() => {
+								setToggleMenu(false);
+							}}
+						/>
+
+						<ul className="app__navbar-smallscreen_links">
+							<li>
+								<a
+									href="#"
+									onClick={() => {
+										setToggleMenu(false);
+									}}
+								>
+									Home
+								</a>
+							</li>
+							<li>
+								<a
+									href="#about"
+									onClick={() => {
+										setToggleMenu(false);
+									}}
+								>
+									About
+								</a>
+							</li>
+							<li>
+								<a
+									href="#menu"
+									onClick={() => {
+										setToggleMenu(false);
+									}}
+								>
+									Menu
+								</a>
+							</li>
+							<li>
+								<a
+									href="#awards"
+									onClick={() => {
+										setToggleMenu(false);
+									}}
+								>
+									Awards
+								</a>
+							</li>
+							<li>
+								<a
+									href="#contact"
+									onClick={() => {
+										setToggleMenu(false);
+									}}
+								>
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				)}
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
